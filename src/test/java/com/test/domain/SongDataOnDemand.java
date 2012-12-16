661c8f8df91c57b39449ec31d12e90d569af9961
Rafael Yanez <rafaelyanez@gmail.com>
2012-12-16 18:18:32 -0500

    new files

---
+++ src/test/java/com/test/domain/SongDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.test.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Song.class)
+public class SongDataOnDemand {
+}
---
+++ src/test/java/com/test/domain/SongIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.test.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Song.class)
+public class SongIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
